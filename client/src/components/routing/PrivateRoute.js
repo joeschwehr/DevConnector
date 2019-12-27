ecf55c283669dfa02c6e8a750e6a63005b1a30f4
Joe <joeschwehr@gmail.com>
2019-12-27 11:54:39 -0800

    improved private route access

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 
 const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading }, ...rest }) => (
     <Route
         {...rest}
         render={props =>
-            !isAuthenticated && !loading ? <Redirect to='/login' /> : <Component {...props} />
+            !loading ? (
+                !isAuthenticated ? (
+                    <Redirect to='/login' />
+                ) : (
+                    <Component {...props} />
+                )
+            ) : (
+                <Spinner />
+            )
         }
     />
 );
