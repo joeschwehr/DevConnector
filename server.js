1468a235faad147d871d0c575d089d8a51958873
Joe <joeschwehr@gmail.com>
2019-12-27 11:14:57 -0800

    prepared for deployment

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-//connect to mongoDB
+// connect to mongoDB
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Hi, there, courtland');
-});
-
-//init middleware... formerly bodyparser
+// init middleware... formerly bodyparser
 app.use(express.json({ extended: false }));
 
 //define routes
@@ -18,6 +15,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// server static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
